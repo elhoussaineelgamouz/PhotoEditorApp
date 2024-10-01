35a6c2997f479ca4c5dc3c770280074f027f4a81
elhoussaine elgamouz <elhoussaineelgamouz@gmail.com>
2024-10-01 13:22:32 +0100

    Initial Commit

---
+++ PhotoEditorApp/PhotoEditorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoEditorAppApp.swift
+//  PhotoEditorApp
+//
+//  Created by El houssaine El GAMOUZ on 1/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct PhotoEditorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
